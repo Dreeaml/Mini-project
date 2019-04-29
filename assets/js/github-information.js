5da2578d7421af486e6281c840d6286dd3531ede
Andrea Maggino <andreamaggino99@gmail.com>
2019-04-29 12:26:18 +0000

    Fetch repo information for users

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -14,6 +14,25 @@ function userInformationHTML(user) {
             <p>Followers: ${user.followers} - Following ${user.following} <br> Repos: ${user.public_repos}</p>
         </div>`;
 }
+
+function repoInformationHTML(repos){
+    if(repos.lengh == 0){
+        return `<div class="clearfix repo-list">No repos!</div>`;
+    }
+    var listItemsHTML = repos.map(function(repo){
+        return `<li>
+            <a href="${repo.html_url}" target="_blank">${repo.name}</a>
+        </li>`;
+    });
+    return `<div class="clearfix repo-list">
+        <p>
+            <strong>Repo List:</strong>
+            <ul>
+                ${listItemsHTML.join("/n/")}
+            </ul>
+        </p>
+    </div>`;
+}
 function fetchGitHubInformation(event) {
 
     var username = $("#gh-username").val();
@@ -29,10 +48,13 @@ function fetchGitHubInformation(event) {
         
     $.when(
         $.getJSON(`https://api.github.com/users/${username}`)
+        $.getJSON(`https://api.github.com/users/${username}/repos`)
         ).then(
-            function (response){
-                var userData = response;
+            function (firstResponse, secondResponse){
+                var userData = firstResponse[0];
+                var repoData = secondResponse[0];
                 $("#gh-user-data").html(userInformationHTML(userData));
+                $("#gh-repo-data").html(userInformationHTML(repoData));
                 
             }, function (errorResponse){
                 if(errorResponse.status === 404){
