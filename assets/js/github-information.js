2abce0fad929ab51f8c2efeb42193275f63ca388
Andrea Maggino <andreamaggino99@gmail.com>
2019-04-29 17:05:52 +0000

    Implement emailjs integration on the contact page

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -61,6 +61,9 @@ function fetchGitHubInformation(event) {
             }, function (errorResponse){
                 if(errorResponse.status === 404){
                     $("gh-user-data").html(`<h2>No info found for user ${username}</h2>`);
+                }else if(errorResponse.status === 403){
+                    var restTime = new Date(errorResponse.getResponseHeader(`X-RateLimit-Reset header`)*1000);
+                    $("#gh-user-data").html(`<h4>To many requests, please wait until ${restTime.toLocaleTimeString()}</h4>`);
                 }else{
                     console.log(errorResponse);
                     $("#gh-user-data").html(`<h2>Error: ${errorResponse.responseJSON.message}</h2>`);
